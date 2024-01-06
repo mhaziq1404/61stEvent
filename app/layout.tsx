7e771d56f966295dd374766b013956c2cec0f33f
Muhamad Haziq <haziq@Macs-MacBook-Pro.local>
2024-01-06 13:22:02 +0800

    MVP completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
+  title: '61stEvent',
+  description: '61stEvent is a platform for event management.',
   icons: {
     icon: '/assets/images/logo.svg'
   }
